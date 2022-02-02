74d7605f25a34c42465832625463b47d376c4b4b
Dylan Duault <dylan.duault@gmail.com>
2022-02-02 20:24:22 +0100

    First commit

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version "cerulean"
+
+description "Zonama website, available from an interaction with a computer"
+author "Dylan Duault"
+version '1.0.0'
+
+lua54 'yes'
+
+games {
+  "gta5"
+}
+
+ui_page 'web/build/index.html'
+
+client_script "client/**/*"
+server_script "server/**/*"
+
+files {
+  'web/build/index.html',
+  'web/build/**/*'
+}
